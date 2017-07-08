32a437d104a6915fd01577b42881063ac981800b
Dale Pollitt <dale@homebrewedcode.com>
2017-07-08 17:23:55 +0000

    edit gemfile for postgres

--- Gemfile
+++ Gemfile
@@ -47,3 +47,8 @@ group :development do
   gem 'spring'
 end
 
+group :production do
+  gem 'pg'
+end
+
+
---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,41 @@
+class MoviesController < ApplicationController
+    def index
+        @movies = Movie.all
+    end
+    
+    def show
+        id = params[:id]
+        @movie = Movie.find(id)
+    end
+    
+    def new
+        @movie = Movie.new
+    end
+    
+    def create 
+        params.require(:movie)
+        permitted = params[:movie].permit(:title, :rating, :release_date)
+        @movie = Movie.create!(permitted)
+        flash[:notice] = "#{@movie.title} was successfully created"
+        redirect_to movies_path
+    end
+    
+    def edit
+        @movie = Movie.find params[:id]
+    end
+    
+    def update
+        @movie = Movie.find params[:id]
+        permitted = params[:movie].permit(:title, :rating, :release_date)
+        @movie.update_attributes!(permitted)
+        flash[:notice] = "#{@movie.title} updated successfully"
+        redirect_to movies_path
+    end
+    
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        flash[:notice] = "Movie #{@movie.title} successfully deleted"
+        redirect_to movies_path
+    end
+end
